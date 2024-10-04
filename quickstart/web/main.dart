d865c46fc8e23e3b3cddc71dc06368c188099976
Austin Valentine <1627989+austinav@users.noreply.github.com>
2024-10-03 23:46:47 -0500

    Finish basic dart web app example

--- quickstart/web/main.dart
+++ quickstart/web/main.dart
@@ -5,4 +5,24 @@ void main() {
   final element = web.document.querySelector('#output') as web.HTMLDivElement;
   element.text = 'The time is ${now.hour}:${now.minute} '
       'and your Dart web app is running!';
+
+  for (final item in thingsTodo()) {
+    element.appendChild(newLI(item));
+  }
 }
+
+Iterable<String> thingsTodo() sync* {
+  const actions = ['Walk', 'Wash', 'Feed'];
+  const pets = ['cats', 'dogs'];
+
+  for (final action in actions) {
+    for (final pet in pets) {
+      if (pet != 'cats' || action == 'Feed') {
+        yield '$action the $pet';
+      }
+    }
+  }
+}
+
+web.HTMLLIElement newLI(String itemText) =>
+    (web.document.createElement('li') as web.HTMLLIElement)..text = itemText;
